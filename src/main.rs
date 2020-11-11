19b7bde0301e44fed6401b9b9a8085ae2af539d9
neil <neewill@gmail.com>
2020-11-11 15:10:00 +0000

    Replace enum with match / and box inline

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-use compression::Algorithm;
 use std::{env, fs::File, io};
 
 mod block_compress;
 mod compression;
 mod lz77;
-
-enum CompressionAlgorithm {
-    Block,
-    Lz77,
-}
-
-impl CompressionAlgorithm {
-    fn create(&self) -> Box<dyn Algorithm> {
-        match *self {
-            CompressionAlgorithm::Block => Box::new(block_compress::BlockCompression {}),
-            CompressionAlgorithm::Lz77 => Box::new(lz77::Lz77Compression {}),
-        }
-    }
-}
 
 /// a really rubbish file compressor.
 ///
@@ -34,12 +19,13 @@ fn main() -> io::Result<()> {
     let output_path = &args[4];
     println!("{:?}", args);
 
-    let algorithm = match algo.as_str() {
-        "block" => CompressionAlgorithm::Block,
-        "lz77" => CompressionAlgorithm::Lz77,
+    // I wonder what the compiler outputs for this?
+    // We could avoid the heap alloc if we just had if/else blocks.
+    let compressor: Box<dyn compression::Algorithm> = match algo.as_str() {
+        "block" => Box::new(block_compress::BlockCompression {}),
+        "lz77" => Box::new(lz77::Lz77Compression {}),
         _ => panic!("Unknown compression algorithm"),
     };
-    let compressor = algorithm.create();
 
     let file = File::open(path)?;
 
